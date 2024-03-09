b44f517da1d1b91aefed563424c63ee5005ee2c9
Nikita Shvetsov <nshvtsv@gmail.com>
2024-03-09 05:21:50 +0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { MainLayout } from '@/components/Layout/MainLayout';
 import dynamic from 'next/dynamic';
+import { ipAddress } from '@vercel/edge';
 
 export default function Home() {
   const NotesList = dynamic(() => import('@/components/Notes/NotesList'));
+  const { ip } = ipAddress(request);
 
+  console.log(ip, 'IPADRESS');
   return (
     <MainLayout
       title="Главная страница"
